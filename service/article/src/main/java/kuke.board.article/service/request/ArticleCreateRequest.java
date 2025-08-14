2222f0690e4be7ddcb10d9c42a6397f1d0584a80
je1113 <jje320594@gmail.com>
2025-08-14 22:23:37 +0900

    게시글 CRUD API 구현

---
+++ service/article/src/main/java/kuke.board.article/service/request/ArticleCreateRequest.java
@@ -0,0 +1,14 @@
+package kuke.board.article.service.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ArticleCreateRequest {
+    private String title;
+    private String content;
+    private Long writerId;
+    private Long boardId;
+}
+
